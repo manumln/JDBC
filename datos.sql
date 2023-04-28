536e43bb7512e5dd0e4f3b27388360f5ec446cc5
manumln <mmolruib@g.educaand.es>
2023-04-28 14:12:45 +0200

    SQL datos

---
+++ datos.sql
@@ -0,0 +1,20 @@
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (1, 'John Doe', 12345678, '123 Main St.', '555-1234', 'john@example.com', 'usuario', 'Abc123!@');
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (2, 'Jane Smith', 87654321, '456 Oak Ave.', '555-5678', 'jane@example.com', 'administrador', 'P@ssw0rd');
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (3, 'Bob Johnson', 98765432, '789 Elm St.', '555-4321', 'bob@example.com', 'invitado', '12!@qwEr');
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (4, 'Alice Williams', 23456789, '321 Pine St.', '555-8765', 'alice@example.com', 'usuario', 'P@ssword123');
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (5, 'Mark Lee', 34567890, '654 Maple St.', '555-2345', 'mark@example.com', 'usuario', 'Qwe!@456');
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (6, 'Karen Kim', 78901234, '987 Oak St.', '555-9012', 'karen@example.com', 'administrador', 'Abc12345!@');
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (7, 'David Davis', 45678901, '456 Elm St.', '555-3456', 'david@example.com', 'usuario', 'Password1!');
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (8, 'Anna Lee', 90123456, '321 Maple Ave.', '555-6789', 'anna@example.com', 'invitado', '1qA!@qwe');
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (9, 'Mike Brown', 56789012, '654 Pine Ave.', '555-7890', 'mike@example.com', 'usuario', 'QweRty12!');
+INSERT INTO usuarios (id_usuario, nombre, dni, direccion, telefono, email, rol, contrasenna)
+VALUES (10, 'Sara Garcia', 34567890, '789 Oak St.', '555-9012', 'sara@example.com', 'usuario', 'P@ssword1234');
