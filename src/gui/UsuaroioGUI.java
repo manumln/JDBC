052ead0b7bc3bf92605e83f77ac3eb2f7aef83ae
manumln <mmolruib@g.educaand.es>
2023-05-22 13:38:46 +0200

    Creacion GUI

---
+++ src/gui/UsuaroioGUI.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class UsuaroioGUI {
+    private JButton Anterior;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField1;
+    private JButton aceptarButton;
+}
