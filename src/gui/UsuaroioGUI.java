112ead3ebb2596b2a24a9172524ddcb2d535fdba
matinal <irebercab@gmail.com>
2023-05-22 14:30:33 +0200

    Actualizacion GUI

--- src/gui/UsuaroioGUI.java
+++ src/gui/UsuaroioGUI.java
@@ -3,9 +3,12 @@ package gui;
 import javax.swing.*;
 
 public class UsuaroioGUI {
-    private JButton Anterior;
     private JTextField textField6;
     private JTextField textField7;
     private JTextField textField1;
-    private JButton aceptarButton;
+    private JButton INSERTARButton;
+    private JButton BORRARButton;
+    private JButton button3;
+    private JButton button4;
+    private JButton ACTUALIZARButton;
 }
